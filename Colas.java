dc1de29b515e1317c26349a4f6d724b77d008100
Oswald250 <ipfc20@gmail.com>
2025-12-03 15:00:33 -0600

    codigo nuevo

---
+++ Colas.java
@@ -0,0 +1,17 @@
+public class Colas {
+
+    public static void main(String[] args) {
+
+    double lambda1 = 15,mu1 = 20;
+    double P1 = lambda1 / mu1;
+    double Lq1 = (P1 * P1) / (1 - P1);
+    double Wq1 = Lq1 / lambda1;
+    double W1 = 1 / (mu1 - lambda1);
+
+    System.out.println("");
+    System.out.println("p");
+
+
+        
+    }
+}
